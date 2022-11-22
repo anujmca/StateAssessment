9b03ebeba09667764f787e37bf11596271d3167d
anuj.mca <anuj.mca@gmail.com>
2022-11-22 06:46:42 +0530

    timeout: matching login page ui to prototype

--- StateAssessment/Models/Inventory.cs
+++ StateAssessment/Models/Inventory.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace StateAssessment.Models
 {
+    [Table("Inventory")]
     public partial class Inventory
     {
         public Inventory()
